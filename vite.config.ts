b378d774105bebc7901f3433d5b2c1eedb4b318b
justinejs99 <justinejoy99@gmail.com>
2026-05-03 22:58:40 +0800

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/JodieJustine/', // Set the base path to match the folder name in public_html
   build: {
     rollupOptions: {
       input: {
